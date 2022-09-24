74d3b00a898633e0bd38cc9d210119da1af9ad86
juanmacar15 <jdmadrid19@misena.edu.co>
2022-09-23 20:48:46 -0500

    ejercicio 2

--- main.js
+++ main.js
@@ -4,12 +4,23 @@ addEventListener("DOMContentLoaded", (e) => {
         e.preventDefault();
         validar();
         function validar(){
-            let numero = document.getElementById("num").value;
-            if(numero > 0){
-                document.getElementById('res').innerHTML = 'el numero es positivo';
-            }else if(numero < 0){
-                document.getElementById('res').innerHTML = 'el numero es negativo';
+            let n1 = document.getElementById("num1").value;
+            let n2= document.getElementById("num2").value;
+            let n3 = document.getElementById("num3").value;
+
+            if((n1 > n2) && (n1 > n3)){
+                document.getElementById('res').innerHTML = n1 + ' es el mayor';
             }
+            else if((n2 > n1) && (n2 > n3)){
+                document.getElementById('res').innerHTML = n2 + ' es el mayor';
+            }
+            else if((n3 > n1) && (n3 > n2)){
+                document.getElementById('res').innerHTML = n3 + ' es el mayor';
+            }
+            else{
+                document.getElementById('res').innerHTML = 'hay numeros iguales';
+            }
+
         }
     })
 
