332b926bb2f4ec79b7931ca7566c8725002b512d
juanmacar15 <jdmadrid19@misena.edu.co>
2022-09-27 17:40:33 -0500

    ejercicio 3

--- main.js
+++ main.js
@@ -8,19 +8,34 @@ addEventListener("DOMContentLoaded", (e) => {
             let n2= document.getElementById("num2").value;
             let n3 = document.getElementById("num3").value;
 
-            if((n1 > n2) && (n1 > n3)){
-                document.getElementById('res').innerHTML = n1 + ' es el mayor';
+            if((n1 > n2) && (n1 > n3) && (n2 > n3)){
+                document.getElementById('res1').innerHTML = n1 + ' es el mayor';
+                document.getElementById('res2').innerHTML = n3 + ' es el menor';
             }
-            else if((n2 > n1) && (n2 > n3)){
-                document.getElementById('res').innerHTML = n2 + ' es el mayor';
+            else if((n1 > n2) && (n1 > n3) && (n3 > n2)){
+                document.getElementById('res1').innerHTML = n1 + ' es el mayor';
+                document.getElementById('res2').innerHTML = n2 + ' es el menor';
             }
-            else if((n3 > n1) && (n3 > n2)){
-                document.getElementById('res').innerHTML = n3 + ' es el mayor';
+            else if((n2 > n1) && (n2 > n3) && (n3 > n1)){
+                document.getElementById('res1').innerHTML = n2 + ' es el mayor';
+                document.getElementById('res2').innerHTML = n1 + ' es el menor';
+            }
+            else if((n2 > n1) && (n2 > n3) && (n1 > n3)){
+                document.getElementById('res1').innerHTML = n2 + ' es el mayor';
+                document.getElementById('res2').innerHTML = n3 + ' es el menor';
+            }
+            else if((n3 > n1) && (n3 > n2) && (n1 > n2)){
+                document.getElementById('res1').innerHTML = n3 + ' es el mayor';
+                document.getElementById('res2').innerHTML = n2 + ' es el menor';
+            }
+            else if((n3 > n1) && (n3 > n2) && (n2 > n1)){
+                document.getElementById('res1').innerHTML = n3 + ' es el mayor';
+                document.getElementById('res2').innerHTML = n1 + ' es el menor';
             }
             else{
-                document.getElementById('res').innerHTML = 'hay numeros iguales';
+                document.getElementById('res1').innerHTML = ' hay numeros iguales';
             }
-
+            
         }
     })
 
