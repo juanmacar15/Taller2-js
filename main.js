3871100d14c650d8b90bd55657d236bbd8d41771
juanmacar15 <jdmadrid19@misena.edu.co>
2022-09-28 20:54:11 -0500

    ejercicio 5

--- main.js
+++ main.js
@@ -4,23 +4,16 @@ addEventListener("DOMContentLoaded", (e) => {
         e.preventDefault();
         validar();
         function validar(){
-            let letra = document.getElementById("num1").value;
+            let l1 = document.getElementById("num1").value;
+            let l2 = document.getElementById("num2").value;
 
-            if(letra =="t"){
-                let b = parseInt(prompt("ingrese la base:"));
-                let h = parseInt(prompt("ingrese la altura:"));
-                let at = b*h/2;
-                document.getElementById('res1').innerHTML = 'el area es: ' + at;
+            if((l1 > 0) && (l2 > 0)){
+                let area = l1 * l2;
+                document.getElementById('res1').innerHTML = 'el area es: ' + area;
 
-            }
-            else if(letra =="c"){
-                let r = parseInt(prompt("ingrese el radio:"));
-                const pi =3.1416;
-                let ac = pi*r*r;
-                document.getElementById('res1').innerHTML = 'el area es: ' + ac;
             }
             else{
-                document.getElementById('res1').innerHTML = 'la letra ingresada no es valida';
+                document.getElementById('res1').innerHTML = 'los lados deben ser positivos :)';
             }
 
         }
