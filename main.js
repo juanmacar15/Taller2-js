67acea33f69cee28984d54d517e32c7ee178fe1b
juanmacar15 <jdmadrid19@misena.edu.co>
2022-10-04 17:00:39 -0500

    ejercicio 9

--- main.js
+++ main.js
@@ -4,20 +4,24 @@ addEventListener("DOMContentLoaded", (e) => {
         e.preventDefault();
         validar();
         function validar(){
-            let tra = document.getElementById("num1").value;
-            let h = document.getElementById("num2").value;
+            let a = document.getElementById("num1").value;
+            let b = document.getElementById("num2").value;
+            let c = document.getElementById("num2").value;
+            let d = document.getElementById("num2").value;
 
-            if( tra == "p" || tra == "P"){
-                let form = h * 20000;
-                document.getElementById("res1").innerHTML = 'su pago es de '+form + ' pesos';
+            let a1 = a%2;
+            let b1 = b%2;
+            let c1 = c%2;
+            let d1 = d%2;
 
-            }
-            else if(tra == "a" || tra == "A"){
-                let form = h * 10000;
-                document.getElementById("res1").innerHTML = 'su pago es de '+form + ' pesos';
+            if((a1 == 0) && (b1 == 0) && (c1 == 0) && (d1 == 0)){
+                let suma = a + b + c + d;
+                document.getElementById("res1").innerHTML = 'la suma es: '+suma;
+
             }
             else{
-                document.getElementById("res1").innerHTML = 'la letra ingresada no es correcta';
+                let mult = a * b * c * d;
+                document.getElementById("res2").innerHTML = 'la multiplicacion es : '+mult;
             }
 
         }
